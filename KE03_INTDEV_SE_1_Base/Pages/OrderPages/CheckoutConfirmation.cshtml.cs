9cf8ad6207a48a003ccab8d70e4203bfea0dc3ef
ThatDevFoxy <ThatDevFoxy@gmail.com>
2025-05-22 20:45:38 +0200

    Checkout page
    - Added checkout page
    - Added a confirmation page as per feedback Tom
    - Forced options to be selected
    
    Should work? adding order history next so if any issues arise with this will be fixed there

---
+++ KE03_INTDEV_SE_1_Base/Pages/OrderPages/CheckoutConfirmation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Webshop.Pages.OrderPages
+{
+    public class CheckoutConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
